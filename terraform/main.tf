4a03a8c6e3361df0f14f20a9064fe62e065c555c
David Young <davidy@funkypenguin.co.nz>
2020-05-14 11:40:18 +1200

    Add begining of Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+# Configure the GitHub Provider
+provider "github" {
+  token        = "${var.github_token}"
+  organization = "${var.github_organization}"
+}
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+# This file gets checked into the repo
+# MAKE SURE NO SECRETS GO IN HERE
+
+# My GitHub organization
+organization="geek-cookbook"
+
